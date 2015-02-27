a4473ff438ace61dfb1aa2e205fc0edae3acc425
Vishnu Valleru <vishnuvalleru@gmail.com>
2015-02-27 17:55:15 -0500

    code cleanup

--- GenomicRangeQuery.py
+++ GenomicRangeQuery.py
@@ -1,33 +1,42 @@
-def solution(S, P, Q):
+def solution(S, P, Q):    
+    nextOccurrencePositions = recordNextOccurrencePosition(S)
+    return getQueryResults(P, Q, nextOccurrencePositions)
+
+def recordNextOccurrencePosition(S):
     # a table to record the last occurrences of the indexes
-    table = [[-1] * len(S) for i in range(4)]
-    result = [0] * len(Q)
-    
+    nextOccurrencePositions = [[-1] * len(S) for i in range(4)]
+
     for index in xrange(len(S) - 1, -1, -1):
-        # record the indexes in the reverse order
-        table[0][index] = table[0][(index + 1) % len(S)]
-        table[1][index] = table[1][(index + 1) % len(S)]
-        table[2][index] = table[2][(index + 1) % len(S)]
-        table[3][index] = table[3][(index + 1) % len(S)]
+        # record the next occurrence position
+        nextOccurrencePositions[0][index] = nextOccurrencePositions[0][(index + 1) % len(S)]
+        nextOccurrencePositions[1][index] = nextOccurrencePositions[1][(index + 1) % len(S)]
+        nextOccurrencePositions[2][index] = nextOccurrencePositions[2][(index + 1) % len(S)]
+        nextOccurrencePositions[3][index] = nextOccurrencePositions[3][(index + 1) % len(S)]
         
         if S[index] == 'A':
-            table[0][index] = index
+            nextOccurrencePositions[0][index] = index
         elif S[index] == 'C':
-            table[1][index] = index
+            nextOccurrencePositions[1][index] = index
         elif S[index] == 'G':
-            table[2][index] = index
+            nextOccurrencePositions[2][index] = index
         else:
-            table[3][index] = index
+            nextOccurrencePositions[3][index] = index
     
-    for query in xrange(len(P)):
-        # check from the minimum possible value, if it occurs within the range
-        if P[query] <= table[0][P[query]] <= Q[query]:
-            result[query] = 1
-        elif P[query] <= table[1][P[query]] <= Q[query]:
-            result[query] = 2
-        elif P[query] <= table[2][P[query]] <= Q[query]:
-            result[query] = 3
+    return nextOccurrencePositions
+
+def getQueryResults(begin, end, nextOccurrences):
+    # to store the results of all the queries
+    answers = [0] * len(begin)
+
+    for query in xrange(len(begin)):
+        # check from the minimum possible value, if it occurs within the range inclusive
+        if begin[query] <= nextOccurrences[0][begin[query]] <= end[query]:
+            answers[query] = 1
+        elif begin[query] <= nextOccurrences[1][begin[query]] <= end[query]:
+            answers[query] = 2
+        elif begin[query] <= nextOccurrences[2][begin[query]] <= end[query]:
+            answers[query] = 3
         else:
-            result[query] = 4
+            answers[query] = 4
     
-    return result
+    return answers
--- beta.py
+++ beta.py
@@ -1,21 +1,21 @@
 def solution(A):
-    front_edges = []
-    back_edges = []
+    frontEdges = []
+    backEdges = []
     result = 0
     front_index = 0
     back_index = 0
 
     for index in xrange(len(A)):
         # record the front edges and back edges for each disc
-        front_edges.append(index + A[index])
-        back_edges.append(index - A[index])
+        frontEdges.append(index + A[index])
+        backEdges.append(index - A[index])
 
-    front_edges.sort()
-    back_edges.sort()
+    frontEdges.sort()
+    backEdges.sort()
 
     while front_index < len(A):
         while back_index < len(A) and\
-            back_edges[back_index] <= front_edges[front_index]:
+            backEdges[back_index] <= frontEdges[front_index]:
             back_index += 1
         # for each front edge look at the num of back edges before it (this
         #   means all the back edges are intersections of the front edge),
