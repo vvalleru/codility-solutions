43a98b196d38ad62f3810737777a11f0fcd188e5
Vishnu Valleru <vishnuvalleru@gmail.com>
2015-03-09 10:39:14 -0400

    code cleanup

--- Peaks.py
+++ Peaks.py
@@ -1,24 +1,31 @@
 def solution(A):
     N = len(A)
-    peaks_till_here = [0] * N
-    
+
     if N < 3:
         return 0
     
-    for index in xrange(1, N - 1):
-        peaks_till_here[index] = peaks_till_here[index - 1] 
-        if A[index - 1] < A[index] > A[index + 1]:
-            peaks_till_here[index] += 1
-    peaks_till_here[-1] = peaks_till_here[N - 2]
+    peaks_till_here = getPeaksTillHere(A)
     
     i = 1
     while i <= N:
-        if N % i == 0 and check(peaks_till_here, i):
+        if N % i == 0 and hasPeaksInEachBlock(peaks_till_here, i):
             return N / i
         i += 1
     return 0
-    
-def check(peaks_till_here, block_size):
+
+def getPeaksTillHere(A):
+    N = len(A)
+    peaks_till_here = [0] * N
+
+    for index in xrange(1, N - 1):
+        peaks_till_here[index] = peaks_till_here[index - 1]
+        if A[index - 1] < A[index] > A[index + 1]:
+            peaks_till_here[index] += 1
+    peaks_till_here[-1] = peaks_till_here[N - 2]
+
+    return peaks_till_here
+
+def hasPeaksInEachBlock(peaks_till_here, block_size):
     N = len(peaks_till_here)
     previous_peaks = 0
     i = block_size - 1
