53e0d079b406c5431a313e47a2afe5c520275d07
Vishnu Valleru <vishnuvalleru@gmail.com>
2015-05-05 13:50:58 -0400

    format code

--- FrogRiverOne.py
+++ FrogRiverOne.py
@@ -1,13 +1,13 @@
-def solution(X, A):
-    flags = [False] * (X + 1)
+def solution(finalPosition, leaves):
+  positionsOnTheRiver = [False] * (finalPosition + 1)
     
-    for i in xrange(len(A)):
-        if not flags[A[i]]:
-            # if there is no leaf at the current position,
-            # mark the position and decrement the count of positions to be covered
-            flags[A[i]] = True
-            X -= 1
+  for currentLeaf in xrange(len(leaves)):
+    if not positionsOnTheRiver[leaves[currentLeaf]]:
+      # if there is no leaf at the current position,
+      # mark the position and decrement the count of positions to be covered
+      positionsOnTheRiver[leaves[currentLeaf]] = True
+      finalPosition -= 1
 
-        if X == 0:
-            return i
-    return -1
+    if finalPosition == 0:
+      return currentLeaf
+  return -1
