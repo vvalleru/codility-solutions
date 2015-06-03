399b799b90ced60ebb96b4e12c57114bc217b752
Vishnu Valleru <vishnuvalleru@gmail.com>
2015-06-03 12:45:03 -0400

    enchanced

--- Peaks.py
+++ Peaks.py
@@ -1,37 +1,37 @@
 def solution(A):
     N = len(A)
-
+    
     if N < 3:
         return 0
+        
+    peaks_till_here = get_peaks_till_here(A)
     
-    peaks_till_here = getPeaksTillHere(A)
-    
-    i = 1
-    while i <= N:
-        if N % i == 0 and hasPeaksInEachBlock(peaks_till_here, i):
-            return N / i
-        i += 1
+    for k in range(2, N + 1):
+        if N % k == 0 and check_partition(k, peaks_till_here):
+            return N / k
     return 0
-
-def getPeaksTillHere(A):
-    N = len(A)
-    peaks_till_here = [0] * N
-
-    for index in xrange(1, N - 1):
-        peaks_till_here[index] = peaks_till_here[index - 1]
-        if A[index - 1] < A[index] > A[index + 1]:
-            peaks_till_here[index] += 1
-    peaks_till_here[-1] = peaks_till_here[N - 2]
-
+    
+def get_peaks_till_here(A):
+    peaks_till_here = [0] * len(A)
+    
+    for i in xrange(1, len(A) - 1):
+        peaks_till_here[i] = peaks_till_here[i - 1]
+        
+        if A[i - 1] < A[i] > A[i + 1]:
+            peaks_till_here[i] += 1
+    
+    peaks_till_here[-1] = peaks_till_here[-2]
+    
     return peaks_till_here
-
-def hasPeaksInEachBlock(peaks_till_here, block_size):
-    N = len(peaks_till_here)
-    previous_peaks = 0
-    i = block_size - 1
-    while i < N:
-        if peaks_till_here[i] == previous_peaks:
+    
+def check_partition(k, peaks_till_here):
+    i = len(peaks_till_here) - 1
+    
+    while i - k >= 0:
+        if peaks_till_here[i] <= peaks_till_here[i - k]:
             return False
-        previous_peaks = peaks_till_here[i]
-        i += block_size
+        i -= k
+        
+    if peaks_till_here[k - 1] < 1:
+        return False
     return True
